955a8c0449ca713522b3e5095a46a61e04a4de38
Naman Aggarwal <naman.aggarwal@yahoo.com>
2016-08-06 14:22:01 +0800

    Fixed setup

--- setup.py
+++ setup.py
@@ -3,14 +3,25 @@ from setuptools import setup, find_packages
 INSTALL_REQUIRES = []
 INSTALL_REQUIRES.append('oauth2')
 
+license='MIT'
+if os.path.exists('LICENSE'):
+  license = open('LICENSE').read()
+
+long_description = """
+    The Splitwise SDK provides Splitwise APIs to get data from your Splitwise Account.
+    1. https://github.com/namaggarwal/splitwise - README
+    2. https://github.com/namaggarwal/flask-splitwise-example - Sample
+  """
+
 setup(name='splitwise',
-      version='0.0.1',
+      version='0.0.2',
       description='Splitwise API SDK',
-      long_description='SDK to use Splitwise APIs',
+      long_description=long_description,
       author='Naman Aggarwal',
       author_email='nam.aggarwal@yahoo.com',
-      url='http://namaggarwal.github.io/',
-      license='MIT',
+      url='https://github.com/namaggarwal/splitwise',
+      download_url='https://github.com/namaggarwal/splitwise/tarball/0.0.2',
+      license=license,
       packages=find_packages(),
       classifiers=[
         'Intended Audience :: Developers',
