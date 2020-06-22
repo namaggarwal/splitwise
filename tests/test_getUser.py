ab209ec161566a936c2f8e531459f941656cb6df
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-06-22 23:29:35 +0800

    Tests should run for 2.7

--- tests/test_getUser.py
+++ tests/test_getUser.py
@@ -1,6 +1,9 @@
 from splitwise import Splitwise
 import unittest
-from unittest.mock import patch
+try:
+    from unittest.mock import patch
+except ImportError:  # Python 2
+    from mock import patch
 
 
 @patch('splitwise.Splitwise._Splitwise__makeRequest')
