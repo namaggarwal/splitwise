e41abd999ee123defbb648e2a56799cd9062dd68
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-06-22 23:17:47 +0800

    Added test

---
+++ tests/test_getUser.py
@@ -0,0 +1,23 @@
+from splitwise import Splitwise
+import unittest
+from unittest.mock import patch
+
+
+@patch('splitwise.Splitwise._Splitwise__makeRequest')
+class GetUserTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.sObj = Splitwise('consumerkey', 'consumersecret')
+
+    def test_getUser_success(self, mockMakeRequest):
+
+        mockMakeRequest.return_value = '{"user":{"id":12323,"first_name":"Naman","last_name":"Aggarwal","picture":{"small":"https://splitwise.s3.amazonaws.com/uploads/user/avatar/79774/small_mypic.jpg","medium":"https://splitwise.s3.amazonaws.com/uploads/user/avatar/79774/medium_mypic.jpg","large":"https://splitwise.s3.amazonaws.com/uploads/user/avatar/79774/large_mypic.jpg"},"email":"naman@naman.com","registration_status":"confirmed"}}'.encode(
+            'utf-8')
+        user = self.sObj.getUser(12323)
+
+        mockMakeRequest.assert_called_with(
+            "https://secure.splitwise.com/api/v3.0/get_user/12323")
+        self.assertEqual(user.getId(), 12323)
+        self.assertEqual(user.getFirstName(), "Naman")
+        self.assertEqual(user.getLastName(), "Aggarwal")
+        self.assertEqual(user.getEmail(), "naman@naman.com")
