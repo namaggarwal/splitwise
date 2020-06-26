022def1f9af7861e21a6ee9b851a5e994560a447
Naman Aggarwal <aggarwal.nam@gmail.com>
2020-06-26 17:13:04 +0800

    Added end to end test for group

--- e2e-tests/test_group.py
+++ e2e-tests/test_group.py
@@ -1,14 +1,29 @@
+import unittest
+import os
 from splitwise import Splitwise
 from splitwise.group import Group
-import unittest
+from splitwise.exception import SplitwiseUnauthorizedException
 
 
 class GroupTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.sObj = Splitwise('consumerkey', 'consumersecret', {"oauth_token": "sdsd", "oauth_token_secret": "sdsdd"})
+        consumer_key = os.environ['CONSUMER_KEY']
+        consumer_secret = os.environ['CONSUMER_SECRET']
+        oauth_token = os.environ['OAUTH_TOKEN']
+        oauth_token_secret = os.environ['OAUTH_TOKEN_SECRET']
 
-    def test_group_fail(self):
+        self.sObj = Splitwise(consumer_key, consumer_secret)
+        self.sObj.setAccessToken({'oauth_token': oauth_token, 'oauth_token_secret': oauth_token_secret})
+
+    def test_group_flow(self):
         group = Group()
-        with self.assertRaises(Exception):
-            self.sObj.createGroup(group)
+        group.setName("Splitwise_test_case")
+        group, error = self.sObj.createGroup(group)
+        self.assertIsNotNone(group.getId())
+
+    def test_group_invalidkeys_fail(self):
+        sObj = Splitwise('consumerkey', 'consumersecret', {"oauth_token": "sdsd", "oauth_token_secret": "sdsdd"})
+        group = Group()
+        with self.assertRaises(SplitwiseUnauthorizedException):
+            sObj.createGroup(group)
