aa805f87cc5b549b66604179694e2ddcfc4a9b83
Naman Aggarwal <aggarwal.nam@gmail.com>
2020-06-30 23:40:49 +0800

    Specify master doc explicitely readthedocs/readthedocs.org#2569

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,8 @@ autoclass_content = "both"
 
 autodoc_member_order = "bysource"
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
