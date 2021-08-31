c297aede5a28f41be78fad81588818e09b26823d
Naman Aggarwal <aggarwal.nam@gmail.com>
2021-08-31 21:33:27 +0100

    Upgraded version to 2.3.0

--- splitwise/__version__.py
+++ splitwise/__version__.py
@@ -1,10 +1,10 @@
 __title__ = 'splitwise'
 __description__ = 'Splitwise Python SDK'
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 __url__ = 'https://github.com/namaggarwal/splitwise'
 __download_url__ = 'https://github.com/namaggarwal/splitwise/tarball/v'+__version__
 __build__ = 0x022400
 __author__ = 'Naman Aggarwal'
 __author_email__ = 'aggarwal.nam@gmail.com'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2020 Naman Aggarwal'
+__copyright__ = 'Copyright 2021 Naman Aggarwal'
