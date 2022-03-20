d30b9863fb97cbdf4a971322facca49c6c4f3c22
Daniel O'Connor <daniel.oconnor@gmail.com>
2022-03-20 16:58:38 +1030

    Fix naming

--- splitwise/__init__.py
+++ splitwise/__init__.py
@@ -459,7 +459,7 @@ class Splitwise(object):
                     offset=None,
                     limit=None,
                     group_id=None,
-                    friendship_id=None,
+                    friend=None,
                     dated_after=None,
                     dated_before=None,
                     updated_after=None,
@@ -471,7 +471,7 @@ class Splitwise(object):
             offset(int, optional): Number of expenses to be skipped
             limit(int, optional): Number of expenses to be returned
             group_id(long, optional): GroupID of the expenses
-            friendship_id(long, optional): FriendshipID of the expenses
+            friend_id(long, optional): FriendshipID of the expenses
             dated_after(str, optional): ISO 8601 Date time. Return expenses later that this date
             dated_before(str, optional): ISO 8601 Date time. Return expenses earlier than this date
             updated_after(str, optional): ISO 8601 Date time. Return expenses updated after this date
